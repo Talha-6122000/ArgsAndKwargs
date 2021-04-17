0f116c6d36a0e7f62bab86a396ef98ccef70c6ef
Muhammad Talha <>
2021-04-17 15:04:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Work with Python long enough, and eventually you will encounter `*args` and `**kwargs`. These strange terms show up as parameters in function definitions. What do they do? Let's review a simple function:
+def myfunc(a,b):
+    return sum((a,b))*.05
+
+print(myfunc(40,60))
+# This function returns 5% of the sum of **a** and **b**. In this example, **a** and **b** are *positional* arguments; that is, 40 is assigned to **a** because it is the first argument, and 60 to **b**. Notice also that to work with multiple positional arguments in the `sum()` function we had to pass them in as a tuple.
+
+# What if we want to work with more than two numbers? One way would be to assign a *lot* of parameters, and give each one a default value.
+def myfunc(a=0,b=0,c=0,d=0,e=0):
+    return sum((a,b,c,d,e))*.05
+
+x=myfunc(40,60,20)
+print(x)
